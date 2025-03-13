7ec38f5bf608aeefdaa582dc09a575702e16b348
hunseong-cho <65321347+hunseong-cho@users.noreply.github.com>
2025-03-13 16:42:32 +0900

    Fix: ignore ESLint during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Vercel 빌드 시 ESLint 무시
+  },
 };
 
 export default nextConfig;
+
