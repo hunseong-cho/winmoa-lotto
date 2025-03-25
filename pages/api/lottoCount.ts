1217cfd8733c29f02b70467bc09f4f2a4a382378
hunseong-cho <65321347+hunseong-cho@users.noreply.github.com>
2025-03-25 17:48:39 +0900

    누적자료 노출순서수정

--- pages/api/lottoCount.ts
+++ pages/api/lottoCount.ts
@@ -1,15 +1,17 @@
-// /pages/api/lottoCount.ts
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "@/firebase/firebaseConfig";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     const snapshot = await getDocs(collection(db, "lottoHistory"));
     const count = snapshot.size;
-
-    return res.status(200).json({ count });
+    res.status(200).json({ count });
   } catch (err) {
     console.error("🔥 lottoCount error:", err);
-    return res.status(500).json({ error: "Server error" });
+    res.status(500).json({ error: "Server error" });
   }
 }
