7240f248275d70e94c6af169eccff2b7e42273c5
hunseong-cho <65321347+hunseong-cho@users.noreply.github.com>
2025-03-27 14:27:22 +0900

    버그수정

--- components/LottoGenerator.tsx
+++ components/LottoGenerator.tsx
@@ -830,7 +830,7 @@ const LottoGenerator = () => {
 
 
       {/* ✅ 추가 생성된 번호 (초기화 기능 포함) */}
-      {generatedNumbers.length > 0 && currentAdditionalEntry && (
+      {todayAdditions.length > 0 && currentAdditionalEntry && (
         <div className="w-full max-w-full lg:max-w-[730px] bg-white/60 border border-gray-200 backdrop-blur-md rounded-lg p-4 shadow-md mt-6">
           <div className="text-center text-base md:text-lg lg:text-xl font-semibold text-blue-700 border-b border-blue-200 pb-2 mb-4">
             🎉 추가 생성 완료!{" "}
