feda171666f4ec3eeed383bd09fabbe877ea8904
hunseong-cho <65321347+hunseong-cho@users.noreply.github.com>
2025-03-31 17:00:48 +0900

    20250331_online_pull

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+  /** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://lotto.winmoa.net',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    outDir: "public",
+  };
